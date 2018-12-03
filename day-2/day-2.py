50aea5b6129659703a7eee6af60175f886ee368f
raoulkent <raoulkent@gmail.com>
2018-12-03 08:59:52 +0100

    Minor refactor day-2

--- day-2/day-2.py
+++ day-2/day-2.py
@@ -4,21 +4,24 @@ from difflib import SequenceMatcher
 text_file = open("input", "r")
 id_list = text_file.read().split('\n')
 
+
 # --- Part One ---
-double_char_ids = 0
-triple_char_ids = 0
+def part1(string_list):
+    double_char_ids = 0
+    triple_char_ids = 0
 
-for chars in id_list:
-    char_count = {chars.count(char) for char in set(chars)}
-    if 2 in char_count:
-        double_char_ids += 1
-    if 3 in char_count:
-        triple_char_ids += 1
+    for chars in string_list:
+        char_count = {chars.count(char) for char in set(chars)}
+        if 2 in char_count:
+            double_char_ids += 1
+        if 3 in char_count:
+            triple_char_ids += 1
 
-checksum = double_char_ids * triple_char_ids
+    return double_char_ids * triple_char_ids
+
 
 print('The list of checksum of the list is:')
-print(checksum)
+print(part1(id_list))
 
 
 # --- Part Two ---
